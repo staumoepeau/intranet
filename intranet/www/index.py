94861297096cb93be88bb4dabe5b0c47a3a0d57f
root <root@tal.erpnext>
2019-03-06 09:02:58 +1300

    Update Intranet

--- intranet/www/index.py
+++ intranet/www/index.py
@@ -7,8 +7,8 @@ no_cache = 1
 no_sitemap = 1
 
 def get_context(context):
-     context.operations = frappe.db.sql("select name, file_name, folder_name, file_icon, file_description from `tabDocument Manager` where folder_name='Operations'", as_dict=True)
-     context.finance = frappe.db.sql("select name, file_name, folder_name, file_icon, file_description from `tabDocument Manager` where folder_name='Finance'", as_dict=True)
-     context.intranet = frappe.db.sql("select name, file_name, folder_name, file_attach, file_description from `tabDocument Manager` where folder_name='Intranet'", as_dict=True)
+     context.operations = frappe.db.sql("select name, file_name, folder_name, file_icon, file_description from `tabDocument Manager` where section='Operations'", as_dict=True)
+     context.finance = frappe.db.sql("select name, file_name, folder_name, file_icon, file_description from `tabDocument Manager` where section='Finance'", as_dict=True)
+     context.intranet = frappe.db.sql("select file_name, intranet_title, file_attach, file_description from `tabDocument Manager` where section='Intranet'", as_dict=True)
 
  #    context.finance = frappe.db.sql("select name, file_name, folder_name, file_icon, file_decription from `tabDocument Manager` where folder_name='Finance'", as_dict=True)
