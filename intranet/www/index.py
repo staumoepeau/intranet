e8525ce396e13b5181a5257d24a2b56a443eadc4
root <root@tal.erpnext>
2019-03-07 10:17:12 +1300

    Update

--- intranet/www/index.py
+++ intranet/www/index.py
@@ -7,8 +7,9 @@ no_cache = 1
 no_sitemap = 1
 
 def get_context(context):
-     context.operations = frappe.db.sql("select name, file_name, folder_name, file_icon, file_description from `tabDocument Manager` where section='Operations'", as_dict=True)
-     context.finance = frappe.db.sql("select name, file_name, folder_name, file_icon, file_description from `tabDocument Manager` where section='Finance'", as_dict=True)
-     context.intranet = frappe.db.sql("select file_name, intranet_title, file_attach, file_description from `tabDocument Manager` where section='Intranet'", as_dict=True)
+    context.top_menu = frappe.db.sql("select intranet_menu, intranet_menu_details from `tabIntranet Top Menu`", as_dict=True)
+    context.operations = frappe.db.sql("select name, file_name, folder_name, file_icon, file_description from `tabDocument Manager` where section='Operations'", as_dict=True)
+    context.finance = frappe.db.sql("select name, file_name, folder_name, file_icon, file_description from `tabDocument Manager` where section='Finance'", as_dict=True)
+    context.intranet = frappe.db.sql("select file_name, intranet_title, file_attach, file_description from `tabDocument Manager` where section='Intranet'", as_dict=True)
 
  #    context.finance = frappe.db.sql("select name, file_name, folder_name, file_icon, file_decription from `tabDocument Manager` where folder_name='Finance'", as_dict=True)
